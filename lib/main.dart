b8b463f3664b872808c350c0760268555fc493b5
Baber19 <Baber19i@outlook.com>
2026-02-18 10:14:59 +0500

    Calculator

--- lib/main.dart
+++ lib/main.dart
@@ -291,7 +291,7 @@ class _MyAppState extends State<MyApp> {
                     decoration: BoxDecoration(color: Colors.purple),
                     child: Center(
                       child: Text(
-                        "Calculate BMI",
+                        "Calculate Your BMI",
                         style: TextStyle(
                           color: Colors.white,
                           fontSize: 40,
